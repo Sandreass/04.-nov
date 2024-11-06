e8817b42636e8568f16ebc3343110edc99cc54fc
Andrei Stroianetki <sandreasweb@gmail.com>
2024-11-06 22:56:48 +0100

    adding 3 button

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ const searchInput = document.querySelector("[data-search]");
 
 let users = [];
 
-searchInput.addEventListener("input", e => {
+searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase();
-  users.forEach(user => {
+  users.forEach((user) => {
     const isVisible =
       user.name.toLowerCase().includes(value) ||
       user.email.toLowerCase().includes(value);
     user.element.classList.toggle("hide", !isVisible);
   });
 });
-const url = "https://jsonplaceholder.typicode.com/users";
+const url = "https://jsonplaceholder.typicode.com/users"; //
+
 
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
+    /*console.log(data);*/
     users = data.map((user) => {
       const card = userCardTemplate.content.cloneNode(true).children[0];
       const header = card.querySelector("[data-header]");
@@ -26,5 +28,26 @@ fetch(url)
       body.innerText = user.email;
       userCardContainer.appendChild(card);
       return { name: user.name, email: user.email, element: card };
+    });
+    const sortBtn = document.querySelector(".sort_btn");
+    const gridBtn = document.querySelector(".grid_btn");
+    const listBtn = document.querySelector(".flex_btn");
+
+    sortBtn.addEventListener("click", () => {
+      if (users) {
+        users.sort((a, b) => a.name.localeCompare(b.name));
+        userCardContainer.innerHTML = "";
+        users.forEach((user) => {
+          userCardContainer.appendChild(user.element);
+        });
+      }
+    });
+    gridBtn.addEventListener("click", () => {
+        userCardContainer.classList.remove("user-cards2");
+      userCardContainer.classList.add("user-cards1");
+    });
+    listBtn.addEventListener("click", () => {
+        userCardContainer.classList.remove("user-cards1");
+      userCardContainer.classList.add("user-cards2");
     });
   });
